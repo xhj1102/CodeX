6937ef7f4ebbe7ae17744236c4eb5e94d50e729e
Haoji Xu <haoji.xu@me.com>
2015-02-09 10:53:53 +0800

    Rewritten in C++.

---
+++ CodeX.cpp
@@ -0,0 +1,10 @@
+/**
+ * The main CodeX API.
+ * All methods are passed in.
+ * You can use functions below to code and decode.
+ * Copyright Aurura Int'l. No right reserved.
+ */
+
+char encodeChar(char c, int pass) {
+    
+}
